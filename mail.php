16157a8f7600875d6496f8fa35b88b5580823ff7
Vineet Jakhar <100095098+Vineet-Jakhar@users.noreply.github.com>
2022-07-12 00:38:34 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,56 +1,50 @@
 <?php
 
-if(isset($_POST['submit'])){
-
     $name = $_POST['name'];
     $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-
-    $mailheader = "From:".$name."<".$email.">\r\n";
+    $sub = $_POST['subject'];
+    $msg = $_POST['message'];
 
     $to = "vineetjakhar5432100@gmail.com";
+    $subject='From Portfolio Contact Form';
+    $message="Name: ".$name."\n"."Email: ".$email."\n\n"."Subject: ".$sub."\n\n"."Message: ".$msg;
 
+    $mailheader = "From: ".$email;
+    
+    
     if(mail($to, $subject, $message, $mailheader)){
-        echo"<h1>Sent!!</h1>";
-    } 
+        echo'<!DOCTYPE html>
+        <!-- Created By CodingNepal -->
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Sent</title>
+            <link rel="icon" href="images/icon.jpeg">
+            <link rel="stylesheet" href="style.css">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+            <script src="https://kit.fontawesome.com/38bd9f1332.js" crossorigin="anonymous"></script>
+          
+        </head>
+        <body>
+            <div class="contactsuccess">
+                <h1>Thanks for contacting!</h1>
+                <p>Have a nice day..<br>
+                Go back to <a href="index.html">Homepage</a>
+            </p>
+            </div>
+        </body>
+        </html>';
+    }
     else{
-        echo"Error";
+        echo"Something went wrong :(";
     }
 
 
 
 
+    //     //redirect
+//     header("Location:contactsuccess.html");
+// 
 
-
-
-
-
-
-
-
-
-
-// echo'
-// <!DOCTYPE html>
-// <html lang="en">
-// <head>
-//     <meta charset="UTF-8">
-//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <title>Contact form</title>
-//     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Poppins&display=swap" rel="stylesheet">
-//     <link rel="stylesheet" href="style.css">
-// </head>
-// <body>
-//     <div class="container">
-//         <h1>Thank you for contacting me. I will get back to you as soon as possible!</h1>
-//         <p class="back">Go back to the <a href="index.html">Homepage</a>.</p>
-        
-//     </div>
-// </body>
-// </html>
-// ';
-
-}
 ?>
